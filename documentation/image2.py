d9185bbd7ae0decb6b496073182545b7fad565bb
Aleksandr Andreev <aleksandr.andreev@gmail.com>
2025-07-07 20:34:04 +0200

    Add images

--- documentation/image2.py
+++ documentation/image2.py
@@ -16,17 +16,17 @@ import argparse
 
 # Constants, these are the main "settings" for the image
 WIDTH, HEIGHT, MARGIN, FRAMES = 2048, 1024, 128, 1
-FONT_PATH = "fonts/ttf/RadioCanadaDisplay-Regular.ttf"
+FONT_PATH = "fonts/ttf/EvangelionCPA-Regular.ttf"
 FONT_LICENSE = "OFL v1.1"
 AUXILIARY_FONT = "Helvetica"
 AUXILIARY_FONT_SIZE = 48
 
-LINE_ONE = "ABCDEFGHIJKLMNOPQ"
-LINE_TWO = "RSTUVWXYZ123456789"
-LINE_THREE = "abcdefghijklmnopqrstu"
-LINE_FOUR = "vwxyz,.;:!@#$%^&*(){}[]"
-BIG_TEXT_FONT_SIZE = 160
-BIG_TEXT_SIDE_MARGIN = MARGIN * 1
+LINE_ONE = "ܫܠܡܐ ܥܠܡܐ"
+LINE_TWO = " "
+LINE_THREE = "ܐܢܐ ܐܢܐ ܢܘܗܪܐ ܕܥܠܡܐ"
+LINE_FOUR = " "
+BIG_TEXT_FONT_SIZE = 120
+BIG_TEXT_SIDE_MARGIN = WIDTH - MARGIN * 1
 BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5.45
 
 GRID_VIEW = False # Toggle this for a grid overlay
